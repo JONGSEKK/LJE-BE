2d178e8b85171fbd1f6442be3657e7b6f9fc3516
Rikey Kurniawan Bin Darmaji <121015133+JONGSEKK@users.noreply.github.com>
2023-07-01 00:04:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const { PrismaClient } = require("@prisma/client")
 const cors = require('cors');
 
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
